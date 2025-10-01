9c1984b9150da6740d84ed72c7cf62c40a98b6e5
yimincai <formosaneko.tw@gmail.com>
2025-10-01 08:49:34 +0800

    feat: add all zap field funcs on logger

--- pkg/logger/field.go
+++ pkg/logger/field.go
@@ -1,30 +1,92 @@
 package logger
 
 import (
+	"fmt"
 	"time"
 
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
-// Field is a type alias for zap.Field, which is used to represent structured logging fields.
+// 'Field' is an alias for zap.Field or zapcore.Field
 type Field = zap.Field
 
-func String(key string, val string) Field          { return zap.String(key, val) }
-func Bool(key string, val bool) Field              { return zap.Bool(key, val) }
-func Int(key string, val int) Field                { return zap.Int(key, val) }
-func Int64(key string, val int64) Field            { return zap.Int64(key, val) }
-func Uint(key string, val uint) Field              { return zap.Uint(key, val) }
-func Uint64(key string, val uint64) Field          { return zap.Uint64(key, val) }
-func Float64(key string, val float64) Field        { return zap.Float64(key, val) }
-func Time(key string, val time.Time) Field         { return zap.Time(key, val) }
-func Duration(key string, val time.Duration) Field { return zap.Duration(key, val) }
-func Err(err error) Field                          { return zap.Error(err) }
-func Any(key string, val any) Field                { return zap.Any(key, val) }
-func Strings(key string, val []string) Field       { return zap.Strings(key, val) }
-func Ints(key string, val []int) Field             { return zap.Ints(key, val) }
-func Int64s(key string, val []int64) Field         { return zap.Int64s(key, val) }
-func Float64s(key string, val []float64) Field     { return zap.Float64s(key, val) }
-func Bools(key string, val []bool) Field           { return zap.Bools(key, val) }
-func Reflect(key string, val any) Field            { return zap.Reflect(key, val) }
-func Namespace(key string) Field                   { return zap.Namespace(key) }
-func Skip() Field                                  { return zap.Skip() }
+// --- Basic Types ---
+
+func String(key string, val string) Field     { return zap.String(key, val) }
+func ByteString(key string, val []byte) Field { return zap.ByteString(key, val) }
+func Binary(key string, val []byte) Field     { return zap.Binary(key, val) }
+func Bool(key string, val bool) Field         { return zap.Bool(key, val) }
+
+// --- Integer Types ---
+
+func Int(key string, val int) Field         { return zap.Int(key, val) }
+func Int64(key string, val int64) Field     { return zap.Int64(key, val) }
+func Int32(key string, val int32) Field     { return zap.Int32(key, val) }
+func Int16(key string, val int16) Field     { return zap.Int16(key, val) }
+func Int8(key string, val int8) Field       { return zap.Int8(key, val) }
+func Uint(key string, val uint) Field       { return zap.Uint(key, val) }
+func Uint64(key string, val uint64) Field   { return zap.Uint64(key, val) }
+func Uint32(key string, val uint32) Field   { return zap.Uint32(key, val) }
+func Uint16(key string, val uint16) Field   { return zap.Uint16(key, val) }
+func Uint8(key string, val uint8) Field     { return zap.Uint8(key, val) }
+func Uintptr(key string, val uintptr) Field { return zap.Uintptr(key, val) }
+
+// --- Float and Complex Types ---
+
+func Float64(key string, val float64) Field       { return zap.Float64(key, val) }
+func Float32(key string, val float32) Field       { return zap.Float32(key, val) }
+func Complex128(key string, val complex128) Field { return zap.Complex128(key, val) }
+func Complex64(key string, val complex64) Field   { return zap.Complex64(key, val) }
+
+// --- Pointers (for when value is not guaranteed to exist) ---
+
+func Int64p(key string, val *int64) Field     { return zap.Int64p(key, val) }
+func Int32p(key string, val *int32) Field     { return zap.Int32p(key, val) }
+func Int16p(key string, val *int16) Field     { return zap.Int16p(key, val) }
+func Int8p(key string, val *int8) Field       { return zap.Int8p(key, val) }
+func Uint64p(key string, val *uint64) Field   { return zap.Uint64p(key, val) }
+func Uint32p(key string, val *uint32) Field   { return zap.Uint32p(key, val) }
+func Uint16p(key string, val *uint16) Field   { return zap.Uint16p(key, val) }
+func Uint8p(key string, val *uint8) Field     { return zap.Uint8p(key, val) }
+func Uintptrp(key string, val *uintptr) Field { return zap.Uintptrp(key, val) }
+func Stringp(key string, val *string) Field   { return zap.Stringp(key, val) }
+func Boolp(key string, val *bool) Field       { return zap.Boolp(key, val) }
+func Float64p(key string, val *float64) Field { return zap.Float64p(key, val) }
+func Float32p(key string, val *float32) Field { return zap.Float32p(key, val) }
+
+// --- Time and Error Types ---
+
+func Time(key string, val time.Time) Field           { return zap.Time(key, val) }
+func Timep(key string, val *time.Time) Field         { return zap.Timep(key, val) }
+func Duration(key string, val time.Duration) Field   { return zap.Duration(key, val) }
+func Durationp(key string, val *time.Duration) Field { return zap.Durationp(key, val) }
+func Err(err error) Field                            { return zap.Error(err) }
+func Stack(key string) Field                         { return zap.Stack(key) }
+func StackSkip(key string, skip int) Field           { return zap.StackSkip(key, skip) }
+
+// --- Array/Slice Types ---
+
+func Strings(key string, val []string) Field          { return zap.Strings(key, val) }
+func ByteStrings(key string, val [][]byte) Field      { return zap.ByteStrings(key, val) }
+func Bools(key string, val []bool) Field              { return zap.Bools(key, val) }
+func Ints(key string, val []int) Field                { return zap.Ints(key, val) }
+func Int64s(key string, val []int64) Field            { return zap.Int64s(key, val) }
+func Float64s(key string, val []float64) Field        { return zap.Float64s(key, val) }
+func Times(key string, val []time.Time) Field         { return zap.Times(key, val) }
+func Durations(key string, val []time.Duration) Field { return zap.Durations(key, val) }
+func Errors(key string, errs []error) Field           { return zap.Errors(key, errs) }
+
+// --- Interface and Reflection Types ---
+
+func Stringer(key string, val fmt.Stringer) Field          { return zap.Stringer(key, val) }
+func Object(key string, val zapcore.ObjectMarshaler) Field { return zap.Object(key, val) }
+func Any(key string, val any) Field                        { return zap.Any(key, val) }
+func Reflect(key string, val any) Field                    { return zap.Reflect(key, val) }
+
+// --- Contextual Fields ---
+
+func Namespace(key string) Field               { return zap.Namespace(key) }
+func Dict(key string, val ...Field) Field      { return zap.Dict(key, val...) }
+func Inline(val zapcore.ObjectMarshaler) Field { return zap.Inline(val) }
+func Skip() Field                              { return zap.Skip() }
